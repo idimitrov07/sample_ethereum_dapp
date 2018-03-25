cb97cfd406f89d459b6d326734712b0101b76ef3
Ivan Dimitrov <idimitrov07@gmail.com>
2018-03-25 19:07:28 +0300

    Model a candidate, add candidates to constructor.

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,25 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-  string public candidate;
-  // Constructor
+  // Model a candidate
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
+  // Store candidates
+  // Fetch candidate
+  mapping (uint => Candidate) public candidates;
+  // Store candidate count
+  uint public  candidatesCount;
+
   function Election() public {
-    candidate = "Candidate 1";
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate(string _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
   }
 }
