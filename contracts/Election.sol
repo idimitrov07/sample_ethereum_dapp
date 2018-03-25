afd75fed143014808c877f3095d443be5e56fe58
Ivan Dimitrov <idimitrov07@gmail.com>
2018-03-25 22:23:37 +0300

    Implement voting.

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,6 +7,10 @@ contract Election {
     string name;
     uint voteCount;
   }
+
+  // Store accounts that have voted
+  mapping (address => bool) public voters;
+
   // Store candidates
   // Fetch candidate
   mapping (uint => Candidate) public candidates;
@@ -21,5 +25,18 @@ contract Election {
   function addCandidate(string _name) private {
     candidatesCount++;
     candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  function vote(uint _candidateId) public {
+    // check that the account address has not voted before
+    require(!voters[msg.sender]);
+
+    // require a valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+    // record the voter (address) has voted
+    voters[msg.sender] = true;
+    // update vote count
+    candidates[_candidateId].voteCount++;
   }
 }
